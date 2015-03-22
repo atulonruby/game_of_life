26e2071338cfa7799352f999506f7a700fe30ff5
atulonruby <atuljha86@gmail.com>
2015-03-22 19:37:53 +0530

    Implementation of cell grid done

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,1 +1,48 @@
-#basic file
+class World
+  attr_accessor :rows,:cols,:cell_grid
+  def initialize(rows=3,cols=3)
+    @rows = rows
+    @cols = cols
+    @cell_grid = Array.new(rows) do |row|
+      Array.new(cols) do |col|
+        Cell.new(col,row)
+      end
+    end
+  end
+
+  def live_neighbours_around_cell(cell)
+    live_neighbours = []
+    live_neighbours
+  end
+end
+
+class Cell
+  attr_accessor :alive, :x,:y,:dead
+  def initialize(x=0,y=0)
+    @alive =false
+    @x = x
+    @y = y
+  end
+
+  def alive?
+    alive
+  end
+
+  def dead?
+    !alive
+  end
+end
+
+
+class Game
+  attr_accessor :world,:seeds
+  def initialize(world = World.new,seeds = [])
+    @world = world
+    @seeds = seeds
+
+    seeds.each do |seed| # 8. loop thru,find the cells passed, make them alive
+      world.cell_grid[seed[0]][seed[1]].alive = true
+    end
+  end
+
+end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,5 +1,87 @@
 require 'rspec'
+require_relative 'game_of_life.rb'
 
 describe 'Game of life' do
-  
+  let!(:world){World.new}
+
+  context 'World' do
+    subject {World.new}
+    it 'should create a new world object' do
+      subject.is_a?(World).should be_true
+    end
+
+    it 'should respone to proper methods' do
+      subject.should respond_to(:rows)
+      subject.should respond_to(:cols)
+      subject.should respond_to(:cell_grid)
+      subject.should respond_to(:live_neighbours_around_cell)
+    end
+
+    it 'should  create proper cell_grid on initialization' do
+      subject.cell_grid.is_a?(Array).should be_true
+      subject.cell_grid.each do |row|
+        row.is_a?(Array).should be_true
+        row.each do |col|
+          col.is_a?(Cell).should be_true
+        end
+      end
+    end
+
+    it 'should detect a cell at the North' do
+      subject.cell_grid[0][1].should be_dead
+      subject.cell_grid[0][1].alive = true
+      subject.cell_grid[0][1].should be_alive
+    end
+  end
+
+  context 'Cell' do
+    subject{Cell.new}
+      it 'should create a new cell object' do
+        subject.is_a?(Cell).should be_true
+      end
+      it 'should response to proper methods' do
+        subject.should respond_to(:alive)
+        subject.x.should == 0
+        subject.y.should == 0
+      end
+      it 'should initialize properly' do
+        subject.alive.should be_false
+        subject.should respond_to(:x)
+        subject.should respond_to(:y)
+        subject.should respond_to(:alive?)
+      end
+  end
+
+  context 'Game' do
+    subject{Game.new}
+    it 'should create a new game object' do
+      subject.is_a?(Game).should be_true
+    end
+    it 'should respond to proper methods' do
+      subject.should respond_to(:world)
+      subject.should respond_to(:seeds)
+    end
+    it 'should initialize properly' do
+      subject.world.is_a?(World).should be_true
+      subject.seeds.is_a?(Array).should be_true
+    end
+    it 'should plant seed properly' do
+      game = Game.new(world,[[1,2],[0,2]])
+      world.cell_grid[1][2].should be_alive
+      world.cell_grid[0][2].should be_alive
+    end
+  end
+
+  context 'Rules' do
+    let(:game) {Game.new}
+    context 'Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+      it 'should kill a live cell with 1 live neighbour' do
+        game = Game.new(world,[[1,0],[2,0]])
+        game.tick!
+        world.cell_grid[1][0].should be_dead
+        world.cell_grid[2][0].should be_dead
+      end
+    end
+  end
+
 end
